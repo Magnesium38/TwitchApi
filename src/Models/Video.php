8057918d22f55f63ee160af2e8db338f1fcad8ce
Eric Jensen <magnesium38@gmail.com>
2016-05-08 09:10:34 -0500

    Finish up initial Video model and tests.

--- src/Models/Video.php
+++ src/Models/Video.php
@@ -1,72 +1,167 @@
 <?php namespace MagnesiumOxide\TwitchApi\Model;
 
+/**
+ * @package MagnesiumOxide\TwitchApi\Model
+ */
 class Video extends BaseModel {
+    /**
+     * Returns the title for the video object.
+     *
+     * @return string|null
+     */
     public function getTitle() {
         return $this->getHelper("title");
     }
 
+    /**
+     * Returns the description for the video object.
+     *
+     * @return string|null
+     */
     public function getDescription() {
         return $this->getHelper("description");
     }
 
+    /**
+     * Returns the broadcast id for the video object.
+     *
+     * @return string|null
+     */
     public function getBroadcastId() {
         return $this->getHelper("broadcast_id");
     }
 
+    /**
+     * Returns the status for the video object.
+     *
+     * @return string|null
+     */
     public function getStatus() {
         return $this->getHelper("status");
     }
 
+    /**
+     * Returns the status for the video object.
+     *
+     * @return string|null
+     */
     public function getId() {
         return $this->getHelper("_id");
     }
 
+    /**
+     * Returns the tag list for the video object.
+     *
+     * @return string|null
+     */
     public function getTagList() {
         return $this->getHelper("tag_list");
     }
 
+    /**
+     * Returns the recorded at datetime object for the video object.
+     *
+     * @return \DateTime
+     */
     public function getRecordedAt() {
-        return $this->getHelper("recorded_at");
+        return new \DateTime($this->getHelper("recorded_at"));
     }
 
+    /**
+     * Returns the Game for the video object.
+     *
+     * @return string|null
+     */
     public function getGame() {
         return $this->getHelper("game");
     }
 
+    /**
+     * Returns the length for the video object.
+     *
+     * @return string|null
+     */
     public function getLength() {
         return $this->getHelper("length");
     }
 
+    /**
+     * Returns the preview for the video object.
+     *
+     * @return string|null
+     */
     public function getPreview() {
         return $this->getHelper("preview");
     }
 
+    /**
+     * Returns the url for the video object.
+     *
+     * @return string|null
+     */
     public function getUrl() {
         return $this->getHelper("url");
     }
 
+    /**
+     * Returns the views for the video object.
+     *
+     * @return string|null
+     */
     public function getViews() {
         return $this->getHelper("views");
     }
 
+    /**
+     * Returns the broadcast type for the video object.
+     *
+     * @return string|null
+     */
     public function getBroadcastType() {
         return $this->getHelper("broadcast_type");
     }
 
-    public function getLinks() {
+    /**
+     * Returns the self-referencing links provided by the API.
+     * I don't see a reason to expose this method. Leaving protected in case there's a niche use case.
+     *
+     * @return array|null
+     */
+    protected function getLinks() {
         return $this->getHelper("_links");
     }
 
+    /**
+     * Returns the channel info for the video object
+     *
+     * @return string|null
+     */
     public function getChannelInfo() {
         return $this->getHelper("channel");
     }
 
+    /**
+     * Get a video object with $id.
+     *
+     * @param $id
+     * @return static
+     */
     public static function getVideo($id) {
         $uri = self::buildUri("/videos/:id", ["id" => $id]);
-        $response = self::$client->get($uri);
+        $headers = self::buildHeaders();
+        $response = self::$client->get($uri, [], $headers);
         return static::responseToObject($response);
     }
 
+    /**
+     * Get an array of video objects for the top videos.
+     *
+     * @param int $limit
+     * @param int $offset
+     * @param null $game
+     * @param string $period
+     * @return array
+     */
     public static function getTopVideos($limit = 10, $offset = 0, $game = null, $period = "week") {
         if ($limit > 100) {
             throw new \InvalidArgumentException("Limit cannot be greater than 100.");
@@ -86,7 +181,8 @@ class Video extends BaseModel {
         }
 
         $uri = self::buildUri("/videos/top");
-        $response = self::$client->get($uri, $query);
+        $headers = self::buildHeaders();
+        $response = self::$client->get($uri, $query, $headers);
         return self::responseToArray($response, "videos");
     }
 }
--- tests/model/VideoTest.php
+++ tests/model/VideoTest.php
@@ -1,10 +1,14 @@
 <?php namespace MagnesiumOxide\TwitchApi\Tests\Models;
 
+use MagnesiumOxide\TwitchApi\Model\BaseModel;
 use MagnesiumOxide\TwitchApi\Model\Video;
+use MagnesiumOxide\TwitchApi\Scope;
+use Prophecy\Exception\InvalidArgumentException;
 
 class VideoTest extends BaseTest {
     /** @var Video */
     private $video;
+    private $videoJson;
     protected $class = Video::class;
 
     public function setUp() {
@@ -19,6 +23,7 @@ class VideoTest extends BaseTest {
                 . '"channel":{"name":"twitch","display_name":"Twitch"}}';
         $videoArray = json_decode($videoJson, true);
 
+        $this->videoJson = $videoJson;
         $this->video = Video::create($videoArray);
     }
 
@@ -26,6 +31,118 @@ class VideoTest extends BaseTest {
         $this->assertEquals("Twitch Weekly - February 6, 2015", $this->video->getTitle());
     }
 
-    public function testGetVideo() { $this->markTestIncomplete('This test has not been implemented yet.'); }
-    public function testGetTopVideos() { $this->markTestIncomplete('This test has not been implemented yet.'); }
+    public function testGetDescription() {
+        $this->assertEquals("Twitch Weekly LIVE on February 6, 2015!", $this->video->getDescription());
+    }
+
+    public function testGetBroadcastId() {
+        $this->assertEquals("13019796368", $this->video->getBroadcastId());
+    }
+
+    public function testGetStatus() {
+        $this->assertEquals("recorded", $this->video->getStatus());
+    }
+
+    public function testGetId() {
+        $this->assertEquals("c6055863", $this->video->getId());
+    }
+
+    public function testGetTagList() {
+        $this->assertEquals("", $this->video->getTagList());
+    }
+
+    public function testGetRecordedAt() {
+        $this->assertEquals(new \DateTime("2015-02-06T21:01:09Z"), $this->video->getRecordedAt());
+    }
+
+    public function testGetGame() {
+        $this->assertEquals(null, $this->video->getGame());
+    }
+
+    public function testGetLength() {
+        $this->assertEquals("4015", $this->video->getLength());
+    }
+
+    public function testGetPreview() {
+        $preview = "http://static-cdn.jtvnw.net/jtv.thumbs/archive-621292653-320x240.jpg";
+        $this->assertEquals($preview, $this->video->getPreview());
+    }
+
+    public function testGetUrl() {
+        $this->assertEquals("http://www.twitch.tv/twitch/c/6055863", $this->video->getUrl());
+    }
+
+    public function testGetViews() {
+        $this->assertEquals("318", $this->video->getViews());
+    }
+
+    public function testGetBroadcastType() {
+        $this->assertEquals("highlight", $this->video->getBroadcastType());
+    }
+
+    public function testGetChannelInfo() {
+        $this->assertEquals(["name" => "twitch", "display_name" => "Twitch"], $this->video->getChannelInfo());
+    }
+
+    public function testGetVideo() {
+        $client = $this->mockClient();
+        $config = $this->mockConfig();
+
+        $headers = [
+                "Client-ID" => $config->reveal()["ClientId"],
+                "Accept" => BaseModel::ACCEPT_HEADER,
+        ];
+
+        $body = $this->videoJson;
+
+        $mockedResponse = $this->mockResponse($body, 200);
+
+        $url = BaseModel::BASE_URL . "/videos/c6055863";
+        $client->get($url, [], $headers)
+                ->shouldBeCalled()
+                ->willReturn($mockedResponse);
+
+        $this->assertEquals("c6055863", Video::getVideo("c6055863")->getId());
+    }
+
+    public function testGetTopVideos() {
+        $client = $this->mockClient();
+        $config = $this->mockConfig();
+
+        $headers = [
+                "Client-ID" => $config->reveal()["ClientId"],
+                "Accept" => BaseModel::ACCEPT_HEADER,
+        ];
+
+        $query = [
+            "limit" => 10,
+            "offset" => 0,
+            "period" => "week",
+        ];
+
+        $body = '{"videos":[' . $this->videoJson . ']}';
+
+        $mockedResponse = $this->mockResponse($body, 200);
+
+        $url = BaseModel::BASE_URL . "/videos/top";
+        $client->get($url, $query, $headers)
+                ->shouldBeCalled()
+                ->willReturn($mockedResponse);
+
+        $this->assertEquals("c6055863", Video::getTopVideos()[0]->getId());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetTopVideosThrowsExceptionOnInvalidLimit() {
+        Video::getTopVideos(3000);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetTopVideosThrowsExceptionOnInvalidPeriod() {
+        Video::getTopVideos(10, 0, null, "fortnight");
+    }
 }
