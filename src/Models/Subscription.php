60a037400079e3626f89fd6649f99279f2c3892b
Eric Jensen <magnesium38@gmail.com>
2016-05-22 15:50:51 -0500

    Subscription stuff.

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -1,19 +1,46 @@
 <?php namespace MagnesiumOxide\TwitchApi\Model;
 
 class Subscription extends BaseModel {
+    /**
+     * Returns the internal id.
+     *
+     * @return string|null
+     */
     public function getId() {
         return $this->getHelper("_id");
     }
 
+    /**
+     * Returns the user that the subscription object is for.
+     *
+     * @return null|User
+     */
     public function getUser() {
-        return $this->getHelper("user");
+        return User::create($this->getHelper("user"));
     }
 
+    /**
+     * Returns when the subscription was created.
+     *
+     * @return \DateTime
+     */
     public function getCreatedAt() {
-        return $this->getHelper("created_at");
+        $datetime = $this->getHelper("created_at");
+
+        if (is_null($datetime)) {
+            return null;
+        }
+
+        return new \DateTime($datetime);
     }
 
-    public function getLinks() {
+    /**
+     * Returns the self-referencing links provided by the API.
+     * I don't see a reason to expose this method. Leaving protected in case there's a niche use case.
+     *
+     * @return array|null
+     */
+    protected function getLinks() {
         return $this->getHelper("_links");
     }
 }
--- tests/model/SubscriptionTest.php
+++ tests/model/SubscriptionTest.php
@@ -3,8 +3,36 @@
 use MagnesiumOxide\TwitchApi\Model\Subscription;
 
 class SubscriptionTest extends BaseTest {
+    /** @var Subscription */
+    private $subscription;
+    private $subscriptionJson;
     protected $class = Subscription::class;
-    public function testGetSubscribers() { $this->markTestIncomplete('This test has not been implemented yet.'); }
-    public function testIsSubscribed() { $this->markTestIncomplete('This test has not been implemented yet.'); }
-    public function testGetSubscribedChannel() { $this->markTestIncomplete('This test has not been implemented yet.'); }
+
+    public function setUp() {
+        $subscriptionJson = '{"_id":"88d4621871b7274c34d5c3eb5dad6780c8533318","user":{"_id":38248673,'
+            . '"logo":null,"type":"user","bio":"I\'m testuser","created_at":"2012-12-06T00:32:36Z",'
+            . '"name":"testuser","updated_at":"2013-02-06T21:27:46Z","display_name":"testuser",'
+            . '"_links":{"self":"https://api.twitch.tv/kraken/users/testuser"}},"created_at":"2013-02-06T21:33:33Z",'
+            . '"_links":{"self":"https://api.twitch.tv/kraken/channels/test_channel/subscriptions/testuser"}}';
+        $subscriptionArray = json_decode($subscriptionJson, true);
+
+        $this->subscriptionJson = $subscriptionJson;
+        $this->subscription = Subscription::create($subscriptionArray);
+    }
+
+    public function testGetId() {
+        $this->assertEquals("88d4621871b7274c34d5c3eb5dad6780c8533318", $this->subscription->getId());
+    }
+
+    public function testGetUser() {
+        $this->assertEquals("38248673", $this->subscription->getUser()->getId());
+    }
+
+    public function testGetCreatedAt() {
+        $this->assertEquals(new \DateTime("2013-02-06T21:33:33Z"), $this->subscription->getCreatedAt());
+    }
+
+    public function testInvalidGetCreatedAt() {
+        $this->assertNull(Subscription::create([])->getCreatedAt());
+    }
 }
